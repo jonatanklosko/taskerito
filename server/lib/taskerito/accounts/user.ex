8d3ce17aa522cfb9a811609e6d495b485aefbe58
Jonatan Kłosko <jonatanklosko@gmail.com>
2020-06-02 19:25:47 +0200

    Add user.

---
+++ server/lib/taskerito/accounts/user.ex
@@ -0,0 +1,19 @@
+defmodule Taskerito.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :email, :string
+    field :name, :string
+    field :username, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:username, :name, :email])
+    |> validate_required([:username, :name, :email])
+  end
+end
---
+++ server/priv/repo/migrations/20200602163652_create_users.exs
@@ -0,0 +1,13 @@
+defmodule Taskerito.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :username, :string, null: false
+      add :name, :string, null: false
+      add :email, :string, null: false
+
+      timestamps()
+    end
+  end
+end
