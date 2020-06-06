ebcb453d348df47f2932e652839a67f55a805658
Jonatan Kłosko <jonatanklosko@gmail.com>
2020-06-06 14:57:52 +0200

    Set up Material UI. Add homepage.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import { CssBaseline } from '@material-ui/core';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
+import Home from './Home';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#ff9069',
+      main: '#fc5d3d',
+      dark: '#c22712',
+      contrastText: '#fff',
+    },
+    secondary: {
+      light: '#484848',
+      main: '#212121',
+      dark: '#000000',
+      contrastText: '#fff',
+    },
+  },
+});
 
 function App() {
   return (
-    <img src={logo} alt="logo" style={{ height: '40vmin' }} />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Home />
+    </ThemeProvider>
   );
 }
 
---
+++ client/src/components/Home.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import logo from './logo.svg';
+import { Typography, Grid, Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2),
+    height: '100vh',
+  },
+  rootGrid: {
+    height: '100%',
+  },
+  titleTypography: {
+    fontFamily: 'Indie Flower',
+  },
+  logo: {
+    height: '30vh',
+  },
+}));
+
+function Home() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Grid
+        container
+        direction="column"
+        alignItems="center"
+        justify="center"
+        spacing={3}
+        className={classes.rootGrid}
+      >
+        <Grid item>
+          <img src={logo} alt="logo" className={classes.logo} />
+        </Grid>
+        <Grid item>
+          <Typography
+            variant="h2"
+            className={classes.titleTypography}
+            align="center"
+          >
+            Taskerito
+          </Typography>
+          <Typography
+            variant="h5"
+            className={classes.titleTypography}
+            align="center"
+          >
+            Manage your tasks while eating burrito.
+          </Typography>
+        </Grid>
+        <Grid item container justify="center" spacing={1}>
+          <Grid item>
+            <Button size="large" variant="outlined" color="primary">
+              Sign up
+            </Button>
+          </Grid>
+          <Grid item>
+            <Button
+              size="large"
+              variant="contained"
+              disableElevation
+              color="primary"
+            >
+              Sign in
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+
+export default Home;
